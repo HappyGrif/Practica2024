d36822c0b81d48d5555f2f33c45ad5320f18d05c
HappyGrif <SerafimBear03@gmail.com>
2024-06-22 13:19:48 +0300

    2 api

--- Practica/Practica/Form1.Designer.cs
+++ Practica/Practica/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BelWeb_label = new System.Windows.Forms.Label();
+            this.nbrb_label = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(128, 55);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(128, 229);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(672, 221);
+            this.panel2.TabIndex = 1;
+            // 
+            // BelWeb_label
+            // 
+            this.BelWeb_label.AutoSize = true;
+            this.BelWeb_label.Location = new System.Drawing.Point(179, 58);
+            this.BelWeb_label.Name = "BelWeb_label";
+            this.BelWeb_label.Size = new System.Drawing.Size(35, 13);
+            this.BelWeb_label.TabIndex = 2;
+            this.BelWeb_label.Text = "label1";
+            // 
+            // nbrb_label
+            // 
+            this.nbrb_label.AutoSize = true;
+            this.nbrb_label.Location = new System.Drawing.Point(524, 58);
+            this.nbrb_label.Name = "nbrb_label";
+            this.nbrb_label.Size = new System.Drawing.Size(35, 13);
+            this.nbrb_label.TabIndex = 3;
+            this.nbrb_label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nbrb_label);
+            this.Controls.Add(this.BelWeb_label);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label BelWeb_label;
+        private System.Windows.Forms.Label nbrb_label;
     }
 }
 
--- Practica/Practica/Form1.cs
+++ Practica/Practica/Form1.cs
@@ -1,10 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +13,114 @@ namespace Practica
         {
             InitializeComponent();
         }
-    }
-}
+
+        public class ExchangeRate_BelWeb
+        {
+            public string SourceCurrency { get; set; }
+            public string TargetCurrency { get; set; }
+            public double ExchangeRate { get; set; }
+            public string Direction { get; set; }
+        }
+
+        public class ExchangeRate_nbrb
+        {
+
+            public string Cur_ID { get; set; }
+            public string Date { get; set; }
+            public string Cur_Abbreviation { get; set; }
+            public string Cur_Scale { get; set; }
+            public string Cur_Name { get; set; }
+            public string Cur_OfficialRate { get; set; }
+        }
+
+            private async void BelWebEx()
+            {
+                try
+                {
+                    string apiUrl = "https://kdoapi.bvb.by:8243/currency/v2/banks/BELBBY2X/services?type=currency_exchange";
+
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string content = await response.Content.ReadAsStringAsync();
+                            var exchangeRates = JsonConvert.DeserializeObject<List<ExchangeRate_BelWeb>>(content);
+
+                            var currentRate = exchangeRates.Find(rate =>
+                             rate.SourceCurrency == "USD" &&
+                             rate.TargetCurrency == "BYN");
+
+
+                            if (currentRate != null)
+                            {
+                                BelWeb_label.Text = $"Текущий курс {currentRate.SourceCurrency} к {currentRate.TargetCurrency} ({currentRate.Direction}): {currentRate.ExchangeRate}";
+                            }
+                            else
+                            {
+                                BelWeb_label.Text = "Курс не найден.";
+                            }
+                            // Обработка данных из ответа (в формате JSON)
+                            // Например, десериализация и отображение на форме
+                            // Например:
+                            // dynamic data = JsonConvert.DeserializeObject(content);
+                            // BelWeb_label.Text = data.ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Ошибка: {response.StatusCode}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Исключение: {ex.Message}");
+                }
+            }
+
+        private async void nbrbEx()
+        {
+            try
+            {
+                string apiUrl = "https://api.nbrb.by/exrates/rates/431";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        var exchangeRate = JsonConvert.DeserializeObject<ExchangeRate_nbrb>(content);
+
+                        if (exchangeRate != null)
+                        {
+                            nbrb_label.Text = $"Текущий курс {exchangeRate.Cur_Abbreviation} к BYN: {exchangeRate.Cur_OfficialRate}";
+                            // Обновление интерфейса (например, this.Invalidate())
+                        }
+                        else
+                        {
+                            nbrb_label.Text = "Курс не найден.";
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Ошибка: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Исключение: {ex.Message}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+                BelWebEx();
+                nbrbEx();
+            }
+        }
+ }
+
