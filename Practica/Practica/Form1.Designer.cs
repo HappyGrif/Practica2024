709c021209ac5290660e5526f1204d6471ec8250
HappyGrif <SerafimBear03@gmail.com>
2024-06-23 22:47:26 +0300

    3 api and Exit

--- Practica/Practica/Form1.Designer.cs
+++ Practica/Practica/Form1.Designer.cs
@@ -29,73 +29,296 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button_map = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.button_kyrs = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.BelWeb_label = new System.Windows.Forms.Label();
+            this.BelWeb_USD_label = new System.Windows.Forms.Label();
+            this.nbrb_USD_label = new System.Windows.Forms.Label();
+            this.bb_USD_label = new System.Windows.Forms.Label();
+            this.nbrb_EUR_label = new System.Windows.Forms.Label();
+            this.nbrb_RUB_label = new System.Windows.Forms.Label();
+            this.nbrb_CNY_label = new System.Windows.Forms.Label();
             this.nbrb_label = new System.Windows.Forms.Label();
+            this.BelWeb_label = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.BB_label = new System.Windows.Forms.Label();
+            this.bb_EUR_label = new System.Windows.Forms.Label();
+            this.bb_RUB_label = new System.Windows.Forms.Label();
+            this.bb_CNY_label = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.DarkBlue;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button_map);
+            this.panel1.Controls.Add(this.button_Exit);
+            this.panel1.Controls.Add(this.button_kyrs);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(128, 450);
+            this.panel1.Size = new System.Drawing.Size(116, 692);
             this.panel1.TabIndex = 0;
+            // 
+            // button_map
+            // 
+            this.button_map.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_map.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_map.Location = new System.Drawing.Point(0, 106);
+            this.button_map.Name = "button_map";
+            this.button_map.Size = new System.Drawing.Size(114, 81);
+            this.button_map.TabIndex = 3;
+            this.button_map.Text = "Карта Отделений";
+            this.button_map.UseVisualStyleBackColor = true;
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Exit.Location = new System.Drawing.Point(0, 609);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(114, 81);
+            this.button_Exit.TabIndex = 4;
+            this.button_Exit.Text = "Выход";
+            this.button_Exit.UseVisualStyleBackColor = true;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
+            // button_kyrs
+            // 
+            this.button_kyrs.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_kyrs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_kyrs.Location = new System.Drawing.Point(0, 25);
+            this.button_kyrs.Name = "button_kyrs";
+            this.button_kyrs.Size = new System.Drawing.Size(114, 81);
+            this.button_kyrs.TabIndex = 2;
+            this.button_kyrs.Text = "Курсы валют";
+            this.button_kyrs.UseVisualStyleBackColor = true;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.Red;
+            this.panel3.Controls.Add(this.label1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(128, 55);
+            this.panel3.Size = new System.Drawing.Size(114, 25);
             this.panel3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(0, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Practica2024";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // panel2
             // 
+            this.panel2.BackColor = System.Drawing.Color.SlateBlue;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(128, 229);
+            this.panel2.Location = new System.Drawing.Point(116, 352);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(672, 221);
+            this.panel2.Size = new System.Drawing.Size(1084, 340);
             this.panel2.TabIndex = 1;
             // 
-            // BelWeb_label
+            // BelWeb_USD_label
             // 
-            this.BelWeb_label.AutoSize = true;
-            this.BelWeb_label.Location = new System.Drawing.Point(179, 58);
-            this.BelWeb_label.Name = "BelWeb_label";
-            this.BelWeb_label.Size = new System.Drawing.Size(35, 13);
-            this.BelWeb_label.TabIndex = 2;
-            this.BelWeb_label.Text = "label1";
+            this.BelWeb_USD_label.AutoSize = true;
+            this.BelWeb_USD_label.Location = new System.Drawing.Point(563, 90);
+            this.BelWeb_USD_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BelWeb_USD_label.Name = "BelWeb_USD_label";
+            this.BelWeb_USD_label.Size = new System.Drawing.Size(107, 20);
+            this.BelWeb_USD_label.TabIndex = 2;
+            this.BelWeb_USD_label.Text = "label_BelWeb";
+            // 
+            // nbrb_USD_label
+            // 
+            this.nbrb_USD_label.AutoSize = true;
+            this.nbrb_USD_label.Location = new System.Drawing.Point(281, 90);
+            this.nbrb_USD_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nbrb_USD_label.Name = "nbrb_USD_label";
+            this.nbrb_USD_label.Size = new System.Drawing.Size(83, 20);
+            this.nbrb_USD_label.TabIndex = 3;
+            this.nbrb_USD_label.Text = "label_nbrb";
+            // 
+            // bb_USD_label
+            // 
+            this.bb_USD_label.AutoSize = true;
+            this.bb_USD_label.Location = new System.Drawing.Point(862, 90);
+            this.bb_USD_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bb_USD_label.Name = "bb_USD_label";
+            this.bb_USD_label.Size = new System.Drawing.Size(73, 20);
+            this.bb_USD_label.TabIndex = 4;
+            this.bb_USD_label.Text = "label_BB";
+            // 
+            // nbrb_EUR_label
+            // 
+            this.nbrb_EUR_label.AutoSize = true;
+            this.nbrb_EUR_label.Location = new System.Drawing.Point(281, 137);
+            this.nbrb_EUR_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nbrb_EUR_label.Name = "nbrb_EUR_label";
+            this.nbrb_EUR_label.Size = new System.Drawing.Size(83, 20);
+            this.nbrb_EUR_label.TabIndex = 5;
+            this.nbrb_EUR_label.Text = "label_nbrb";
+            // 
+            // nbrb_RUB_label
+            // 
+            this.nbrb_RUB_label.AutoSize = true;
+            this.nbrb_RUB_label.Location = new System.Drawing.Point(281, 181);
+            this.nbrb_RUB_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nbrb_RUB_label.Name = "nbrb_RUB_label";
+            this.nbrb_RUB_label.Size = new System.Drawing.Size(83, 20);
+            this.nbrb_RUB_label.TabIndex = 6;
+            this.nbrb_RUB_label.Text = "label_nbrb";
+            // 
+            // nbrb_CNY_label
+            // 
+            this.nbrb_CNY_label.AutoSize = true;
+            this.nbrb_CNY_label.Location = new System.Drawing.Point(281, 226);
+            this.nbrb_CNY_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nbrb_CNY_label.Name = "nbrb_CNY_label";
+            this.nbrb_CNY_label.Size = new System.Drawing.Size(83, 20);
+            this.nbrb_CNY_label.TabIndex = 7;
+            this.nbrb_CNY_label.Text = "label_nbrb";
             // 
             // nbrb_label
             // 
             this.nbrb_label.AutoSize = true;
-            this.nbrb_label.Location = new System.Drawing.Point(524, 58);
+            this.nbrb_label.Location = new System.Drawing.Point(281, 40);
+            this.nbrb_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.nbrb_label.Name = "nbrb_label";
-            this.nbrb_label.Size = new System.Drawing.Size(35, 13);
-            this.nbrb_label.TabIndex = 3;
-            this.nbrb_label.Text = "label1";
+            this.nbrb_label.Size = new System.Drawing.Size(53, 20);
+            this.nbrb_label.TabIndex = 8;
+            this.nbrb_label.Text = "НБРБ";
+            // 
+            // BelWeb_label
+            // 
+            this.BelWeb_label.AutoSize = true;
+            this.BelWeb_label.Location = new System.Drawing.Point(563, 40);
+            this.BelWeb_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BelWeb_label.Name = "BelWeb_label";
+            this.BelWeb_label.Size = new System.Drawing.Size(68, 20);
+            this.BelWeb_label.TabIndex = 9;
+            this.BelWeb_label.Text = "БелВеб";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(563, 137);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label_BelWeb";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(563, 181);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label_BelWeb";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(563, 226);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "label_BelWeb";
+            // 
+            // BB_label
+            // 
+            this.BB_label.AutoSize = true;
+            this.BB_label.Location = new System.Drawing.Point(862, 40);
+            this.BB_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.BB_label.Name = "BB_label";
+            this.BB_label.Size = new System.Drawing.Size(122, 20);
+            this.BB_label.TabIndex = 13;
+            this.BB_label.Text = "Беларусь Банк";
+            // 
+            // bb_EUR_label
+            // 
+            this.bb_EUR_label.AutoSize = true;
+            this.bb_EUR_label.Location = new System.Drawing.Point(862, 137);
+            this.bb_EUR_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bb_EUR_label.Name = "bb_EUR_label";
+            this.bb_EUR_label.Size = new System.Drawing.Size(73, 20);
+            this.bb_EUR_label.TabIndex = 14;
+            this.bb_EUR_label.Text = "label_BB";
+            // 
+            // bb_RUB_label
+            // 
+            this.bb_RUB_label.AutoSize = true;
+            this.bb_RUB_label.Location = new System.Drawing.Point(862, 181);
+            this.bb_RUB_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bb_RUB_label.Name = "bb_RUB_label";
+            this.bb_RUB_label.Size = new System.Drawing.Size(73, 20);
+            this.bb_RUB_label.TabIndex = 15;
+            this.bb_RUB_label.Text = "label_BB";
+            // 
+            // bb_CNY_label
+            // 
+            this.bb_CNY_label.AutoSize = true;
+            this.bb_CNY_label.Location = new System.Drawing.Point(862, 226);
+            this.bb_CNY_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bb_CNY_label.Name = "bb_CNY_label";
+            this.bb_CNY_label.Size = new System.Drawing.Size(73, 20);
+            this.bb_CNY_label.TabIndex = 16;
+            this.bb_CNY_label.Text = "label_BB";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateBlue;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.nbrb_label);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.bb_CNY_label);
+            this.Controls.Add(this.bb_RUB_label);
+            this.Controls.Add(this.bb_EUR_label);
+            this.Controls.Add(this.BB_label);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.BelWeb_label);
+            this.Controls.Add(this.nbrb_label);
+            this.Controls.Add(this.nbrb_CNY_label);
+            this.Controls.Add(this.nbrb_RUB_label);
+            this.Controls.Add(this.nbrb_EUR_label);
+            this.Controls.Add(this.bb_USD_label);
+            this.Controls.Add(this.nbrb_USD_label);
+            this.Controls.Add(this.BelWeb_USD_label);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,8 +329,25 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label BelWeb_label;
+        private System.Windows.Forms.Label BelWeb_USD_label;
+        private System.Windows.Forms.Label nbrb_USD_label;
+        private System.Windows.Forms.Label bb_USD_label;
+        private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.Button button_map;
+        private System.Windows.Forms.Button button_kyrs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label nbrb_EUR_label;
+        private System.Windows.Forms.Label nbrb_RUB_label;
+        private System.Windows.Forms.Label nbrb_CNY_label;
         private System.Windows.Forms.Label nbrb_label;
+        private System.Windows.Forms.Label BelWeb_label;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label BB_label;
+        private System.Windows.Forms.Label bb_EUR_label;
+        private System.Windows.Forms.Label bb_RUB_label;
+        private System.Windows.Forms.Label bb_CNY_label;
     }
 }
 
--- Practica/Practica/Form1.cs
+++ Practica/Practica/Form1.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -33,7 +34,20 @@ namespace Practica
             public string Cur_OfficialRate { get; set; }
         }
 
-            private async void BelWebEx()
+        public class ExchangeRate_BB
+        {
+
+            public string kurs_date_time { get; set; }
+            public string USDCARD_in { get; set; }
+            public string USDCARD_out { get; set; }
+            public string EURCARD_in { get; set; }
+            public string EURCARD_out { get; set; }
+            public string RUBCARD_in { get; set; }
+            public string RUBCARD_out { get; set; }
+            public string CNYCARD_in { get; set; }
+            public string CNYCARD_out { get; set; }
+        }
+        private async void BelWebEx()
             {
                 try
                 {
@@ -55,17 +69,13 @@ namespace Practica
 
                             if (currentRate != null)
                             {
-                                BelWeb_label.Text = $"Текущий курс {currentRate.SourceCurrency} к {currentRate.TargetCurrency} ({currentRate.Direction}): {currentRate.ExchangeRate}";
+                                BelWeb_USD_label.Text = $"Текущий курс {currentRate.SourceCurrency} к {currentRate.TargetCurrency}: {currentRate.ExchangeRate}";
                             }
                             else
                             {
-                                BelWeb_label.Text = "Курс не найден.";
+                                BelWeb_USD_label.Text = "Курс не найден.";
                             }
-                            // Обработка данных из ответа (в формате JSON)
-                            // Например, десериализация и отображение на форме
-                            // Например:
-                            // dynamic data = JsonConvert.DeserializeObject(content);
-                            // BelWeb_label.Text = data.ToString();
+                           
                         }
                         else
                         {
@@ -81,9 +91,18 @@ namespace Practica
 
         private async void nbrbEx()
         {
+            nbrb_currency("https://api.nbrb.by/exrates/rates/431");
+            nbrb_currency("https://api.nbrb.by/exrates/rates/451");
+            nbrb_currency("https://api.nbrb.by/exrates/rates/456");
+            nbrb_currency("https://api.nbrb.by/exrates/rates/462");
+
+
+        }
+
+        private async void nbrb_currency(string apiUrl)
+        {
             try
             {
-                string apiUrl = "https://api.nbrb.by/exrates/rates/431";
 
                 using (HttpClient client = new HttpClient())
                 {
@@ -96,12 +115,20 @@ namespace Practica
 
                         if (exchangeRate != null)
                         {
-                            nbrb_label.Text = $"Текущий курс {exchangeRate.Cur_Abbreviation} к BYN: {exchangeRate.Cur_OfficialRate}";
-                            // Обновление интерфейса (например, this.Invalidate())
+                            if (exchangeRate.Cur_Abbreviation == "USD")
+                                nbrb_USD_label.Text = $"Текущий курс {exchangeRate.Cur_Abbreviation} к BYN: {exchangeRate.Cur_OfficialRate}";
+                            if (exchangeRate.Cur_Abbreviation == "EUR")
+                                nbrb_EUR_label.Text = $"Текущий курс {exchangeRate.Cur_Abbreviation} к BYN: {exchangeRate.Cur_OfficialRate}";
+                            if (exchangeRate.Cur_Abbreviation == "RUB")
+                                nbrb_RUB_label.Text = $"Текущий курс {exchangeRate.Cur_Abbreviation} к BYN: {exchangeRate.Cur_OfficialRate}";
+                            if (exchangeRate.Cur_Abbreviation == "CNY")
+                                nbrb_CNY_label.Text = $"Текущий курс {exchangeRate.Cur_Abbreviation} к BYN: {exchangeRate.Cur_OfficialRate}";
+
+
                         }
                         else
                         {
-                            nbrb_label.Text = "Курс не найден.";
+                            nbrb_USD_label.Text = "Курс не найден.";
                         }
                     }
                     else
@@ -115,12 +142,69 @@ namespace Practica
                 MessageBox.Show($"Исключение: {ex.Message}");
             }
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private async void BBEx()
+        {
+            try
             {
-                BelWebEx();
-                nbrbEx();
+                string apiUrl = "https://belarusbank.by/api/kurs_cards";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        var exchangeRates = JsonConvert.DeserializeObject<List<ExchangeRate_BB>>(content);
+
+                        DateTime currentTime = DateTime.Now;
+                        DateTime resetTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 0, 0, 0);
+                        string formattedTime = resetTime.ToString("yyyy-MM-dd HH:mm:ss");
+                        
+
+                        var currentRate = exchangeRates.Find(rate =>
+                                                    rate.kurs_date_time == formattedTime);
+
+
+                        if (currentRate != null)
+                        {
+                            bb_USD_label.Text = $"Текущий курс USD к BYN: {currentRate.USDCARD_out}";
+                            bb_EUR_label.Text = $"Текущий курс EUR к BYN: {currentRate.EURCARD_out}";
+                            bb_RUB_label.Text = $"Текущий курс RUB к BYN: {currentRate.RUBCARD_out}";
+                            bb_CNY_label.Text = $"Текущий курс CNY к BYN: {currentRate.CNYCARD_out}";
+
+                        }
+                        else
+                        {
+                            bb_USD_label.Text = "Курс не найден";
+                            bb_EUR_label.Text = "Курс не найден";
+                            bb_RUB_label.Text = "Курс не найден";
+                            bb_CNY_label.Text = "Курс не найден";
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Ошибка: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Исключение: {ex.Message}");
             }
         }
+        private void Form1_Load(object sender, EventArgs e)
+            {
+            
+            nbrbEx();
+            BBEx();
+            BelWebEx();
+        }
+
+        private void button_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
  }
 
